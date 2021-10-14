e4a641201e860d8d9adb38be59729c192f26f64d
Kharalos <49806742+kharalos@users.noreply.github.com>
2021-10-14 19:31:22 +0300

    Implemented ProductivityUnit

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProductivityUnit : MonoBehaviour
+public class ProductivityUnit : Unit
 {
-    // Start is called before the first frame update
-    void Start()
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplier = 2;
+
+    protected override void BuildingInRange()
     {
-        
+        // start of new code
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+        // end of new code
+    }
+    public override void GoTo(Building target)
+    {
+        ResetProductivity(); // call your new method
+        base.GoTo(target); // run method from base class
+    }
+    public override void GoTo(Vector3 position)
+    {
+        ResetProductivity();
+        base.GoTo(position);
     }
 
-    // Update is called once per frame
-    void Update()
+    void ResetProductivity()
     {
-        
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
     }
 }
